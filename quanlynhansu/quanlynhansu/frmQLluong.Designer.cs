f4807a0c13d657f95f46597f99715b17fd51c039
SCHyperion <79067994+SCHyperion@users.noreply.github.com>
2021-05-18 10:24:12 +0700

    xong lương

--- quanlynhansu/quanlynhansu/frmQLluong.Designer.cs
+++ quanlynhansu/quanlynhansu/frmQLluong.Designer.cs
@@ -36,51 +36,58 @@
             this.txtMaLuong = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgv_List = new System.Windows.Forms.DataGridView();
             this.txtLuongCB = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_List)).BeginInit();
             this.SuspendLayout();
             // 
             // btnXoa
             // 
             this.btnXoa.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnXoa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.btnXoa.Location = new System.Drawing.Point(598, 162);
+            this.btnXoa.Location = new System.Drawing.Point(448, 132);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(88, 33);
+            this.btnXoa.Size = new System.Drawing.Size(66, 27);
             this.btnXoa.TabIndex = 23;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnSua
             // 
             this.btnSua.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSua.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.btnSua.Location = new System.Drawing.Point(439, 162);
+            this.btnSua.Location = new System.Drawing.Point(329, 132);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(88, 33);
+            this.btnSua.Size = new System.Drawing.Size(66, 27);
             this.btnSua.TabIndex = 22;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnThem
             // 
             this.btnThem.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.btnThem.Location = new System.Drawing.Point(279, 162);
+            this.btnThem.Location = new System.Drawing.Point(209, 132);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(88, 33);
+            this.btnThem.Size = new System.Drawing.Size(66, 27);
             this.btnThem.TabIndex = 21;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // txtBacLuong
             // 
-            this.txtBacLuong.Location = new System.Drawing.Point(439, 82);
+            this.txtBacLuong.Location = new System.Drawing.Point(329, 67);
+            this.txtBacLuong.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtBacLuong.Name = "txtBacLuong";
-            this.txtBacLuong.Size = new System.Drawing.Size(165, 22);
+            this.txtBacLuong.Size = new System.Drawing.Size(125, 20);
             this.txtBacLuong.TabIndex = 20;
             // 
             // label1
@@ -88,17 +95,19 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label1.Location = new System.Drawing.Point(275, 82);
+            this.label1.Location = new System.Drawing.Point(206, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 23);
+            this.label1.Size = new System.Drawing.Size(86, 18);
             this.label1.TabIndex = 19;
             this.label1.Text = "Bậc lương:";
             // 
             // txtMaLuong
             // 
-            this.txtMaLuong.Location = new System.Drawing.Point(439, 42);
+            this.txtMaLuong.Location = new System.Drawing.Point(329, 34);
+            this.txtMaLuong.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtMaLuong.Name = "txtMaLuong";
-            this.txtMaLuong.Size = new System.Drawing.Size(165, 22);
+            this.txtMaLuong.Size = new System.Drawing.Size(125, 20);
             this.txtMaLuong.TabIndex = 18;
             // 
             // label2
@@ -106,36 +115,42 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label2.Location = new System.Drawing.Point(275, 41);
+            this.label2.Location = new System.Drawing.Point(206, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 23);
+            this.label2.Size = new System.Drawing.Size(80, 18);
             this.label2.TabIndex = 17;
             this.label2.Text = "Mã lương:";
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Location = new System.Drawing.Point(134, 215);
+            this.panel1.Controls.Add(this.dgv_List);
+            this.panel1.Location = new System.Drawing.Point(100, 175);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(677, 238);
+            this.panel1.Size = new System.Drawing.Size(508, 193);
             this.panel1.TabIndex = 16;
             // 
-            // dataGridView1
+            // dgv_List
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(677, 238);
-            this.dataGridView1.TabIndex = 0;
+            this.dgv_List.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_List.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_List.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_List.Location = new System.Drawing.Point(0, 0);
+            this.dgv_List.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgv_List.Name = "dgv_List";
+            this.dgv_List.RowHeadersWidth = 51;
+            this.dgv_List.RowTemplate.Height = 24;
+            this.dgv_List.Size = new System.Drawing.Size(508, 193);
+            this.dgv_List.TabIndex = 0;
+            this.dgv_List.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_List_CellClick);
             // 
             // txtLuongCB
             // 
-            this.txtLuongCB.Location = new System.Drawing.Point(439, 123);
+            this.txtLuongCB.Location = new System.Drawing.Point(329, 100);
+            this.txtLuongCB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLuongCB.Name = "txtLuongCB";
-            this.txtLuongCB.Size = new System.Drawing.Size(165, 22);
+            this.txtLuongCB.Size = new System.Drawing.Size(125, 20);
             this.txtLuongCB.TabIndex = 25;
             // 
             // label3
@@ -143,18 +158,19 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label3.Location = new System.Drawing.Point(275, 123);
+            this.label3.Location = new System.Drawing.Point(206, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(141, 23);
+            this.label3.Size = new System.Drawing.Size(113, 18);
             this.label3.TabIndex = 24;
             this.label3.Text = "Lương cơ bản:";
             // 
             // frmQLluong
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(946, 504);
+            this.ClientSize = new System.Drawing.Size(710, 410);
             this.Controls.Add(this.txtLuongCB);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnXoa);
@@ -166,11 +182,13 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmQLluong";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmQLluong";
+            this.Text = "Quản Lý Lương";
+            this.Load += new System.EventHandler(this.frmQLluong_Load);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_List)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,7 +204,7 @@
         private System.Windows.Forms.TextBox txtMaLuong;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgv_List;
         private System.Windows.Forms.TextBox txtLuongCB;
         private System.Windows.Forms.Label label3;
     }
--- quanlynhansu/quanlynhansu/frmQLluong.cs
+++ quanlynhansu/quanlynhansu/frmQLluong.cs
@@ -7,14 +7,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace quanlynhansu
 {
     public partial class frmQLluong : Form
     {
+        void init()
+        {
+            string query =  "Select MaLuong as 'Mã Lương', BacLuong as 'Bậc Lương', LuongCB as 'Lương Cơ Bản'" +
+                            " From LUONG" +
+                            " Order by CONVERT(int, right(MaLuong, len(MaLuong)-2)) asc";
+            dgv_List.DataSource = DataProvider.Instance.ExecuteQuery(query);
+        }
         public frmQLluong()
         {
             InitializeComponent();
+        }
+
+        private void frmQLluong_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (txtMaLuong.Text != "")
+            {
+                string query = "Insert into LUONG(MaLuong, BacLuong, LuongCB)" +
+                                "Values('" + txtMaLuong.Text +
+                                "'," + Int32.Parse(txtBacLuong.Text) +
+                                "," + double.Parse(txtLuongCB.Text) + ")";
+                if (DataProvider.Instance.ExecuteNonQuery(query) > 0)
+                {
+                    MessageBox.Show("Thêm lương thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    init();
+                }
+                else
+                    MessageBox.Show("Đã xảy ra lỗi", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Bạn chưa điền mã lương", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (txtMaLuong.Text != "")
+            {
+                string query = "Update LUONG " +
+                                "Set BacLuong = " + Int32.Parse(txtBacLuong.Text) + "," +
+                                "LuongCB = " + double.Parse(txtLuongCB.Text) +
+                                "Where MaLuong = '" + txtMaLuong.Text + "'";
+                if (DataProvider.Instance.ExecuteNonQuery(query) > 0)
+                {
+                    MessageBox.Show("Cập nhật lương thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    init();
+                }
+                else
+                    MessageBox.Show("Đã xảy ra lỗi", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Bạn chưa chọn đối tượng để sửa", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (txtMaLuong.Text != "")
+            {
+                string query = "Delete From LUONG " +
+                                "Where MaLuong = '" + txtMaLuong.Text + "'";
+                string info = txtMaLuong.Text + "  |  " + txtBacLuong.Text + "  |  " + txtLuongCB.Text;
+                DialogResult dr = MessageBox.Show("Bạn có chắc chắn muốn xóa thông tin này ?\n" + info, "Xóa Lương", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (dr == DialogResult.Yes)
+                {
+                    if (DataProvider.Instance.ExecuteNonQuery(query) > 0)
+                    {
+                        MessageBox.Show("Xóa lương thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        init();
+                    }
+                    else
+                        MessageBox.Show("Đã xảy ra lỗi", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }    
+            }
+            else
+                MessageBox.Show("Bạn chưa chọn đối tượng để xóa", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void dgv_List_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMaLuong.Text = dgv_List.CurrentRow.Cells[0].Value.ToString();
+            txtBacLuong.Text = dgv_List.CurrentRow.Cells[1].Value.ToString();
+            txtLuongCB.Text = dgv_List.CurrentRow.Cells[2].Value.ToString();
         }
     }
 }
